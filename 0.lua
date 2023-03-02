c08ee14f19c8fdc23ab6cf6b97d2fbbbcd9cc806
dizzy hub <126523396+dizzyhvh@users.noreply.github.com>
2023-03-02 01:32:32 +0100

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -2,3 +2,5 @@
     This repository is deprecated.
     This script is in new repository: https://github.com/dizzyhvh/rbx_scripts
 ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dizzyhvh/rbx_scripts/main/dizzy_hub/scripts/slap_battles.lua"))();
